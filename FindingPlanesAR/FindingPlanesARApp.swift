1c7cf44886e023ad91e7410537fa6b55f05df9eb
AviramNetanel <aviramdevelopment@gmail.com>
2026-04-13 12:33:13 +0300

    Initial Commit

---
+++ FindingPlanesAR/FindingPlanesARApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingPlanesARApp.swift
+//  FindingPlanesAR
+//
+//  Created by Aviram Netanel on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FindingPlanesARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindingPlanesARTests/FindingPlanesARTests.swift
@@ -0,0 +1,16 @@
+//
+//  FindingPlanesARTests.swift
+//  FindingPlanesARTests
+//
+//  Created by Aviram Netanel on 13/04/2026.
+//
+
+import Testing
+
+struct FindingPlanesARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
