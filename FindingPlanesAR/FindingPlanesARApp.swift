784df5e050906154630ff6febe8ed006ec50d9f2
AviramNetanel <AviramNetanel@gmail.com>
2026-05-17 17:07:37 +0300

    Update FindingPlanesARApp to use FindingPlanesRootView and integrate FindingPlanesSDK into the project configuration.

--- FindingPlanesAR/FindingPlanesARApp.swift
+++ FindingPlanesAR/FindingPlanesARApp.swift
@@ -2,16 +2,15 @@
 //  FindingPlanesARApp.swift
 //  FindingPlanesAR
 //
-//  Created by Aviram Netanel on 13/04/2026.
-//
 
+import FindingPlanesSDK
 import SwiftUI
 
 @main
 struct FindingPlanesARApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FindingPlanesRootView()
         }
     }
 }
